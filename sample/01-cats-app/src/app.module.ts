922105adc676d27cec0d9b639a0e02448874feca
zhaoyu.rei <zhaoyu.rei@bytedance.com>
2024-11-25 14:56:20 +0800

    feat: test

--- sample/01-cats-app/src/app.module.ts
+++ sample/01-cats-app/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
 import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
 
+/**
+ * 根module，可以在此处集成多个模块
+ */
 @Module({
-  imports: [CoreModule, CatsModule],
+  imports: [CoreModule, CatsModule, UserModule],
 })
 export class AppModule {}
